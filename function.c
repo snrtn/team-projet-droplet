a57a50407d2eef0eb2e15340eca5b3b32704ce2d
snrtn <68147027+snrtn@users.noreply.github.com>
2021-04-25 22:11:14 +0200

    Add files via upload

---
+++ function.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int num_total(int a, int b)
+{
+	int result = a + b;
+	return result;
+}
+
+unsigned main()
+{
+	int x, y;
+	scanf("%d%d", &x, &y);
+
+	int total;
+	total = num_total(x, y);
+	printf("%d + %d = %d\n", x, y, total);
+
+	return 0;
+}
