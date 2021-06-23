4b69634b215ee837ac33a003b12b503ec3e4fa08
Junhan <snrtn94@gmail.com>
2021-06-23 20:58:08 +0200

    juni
    asd

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
+// hambuger icon event
 $(function () {
   /* header - sidenav */
   $('.hambuger').click(function () {
     $(this).toggleClass('active');
     $('header').toggleClass('active');
   });
+});
+
+// subname scroll event
+window.addEventListener('scroll', () => {
+  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+  const scrolled = window.scrollY;
+
+  if (Math.ceil(scrolled) === scrollable) {
+    var el = document.getElementById("test");
+    el.style.opacity = '0'
+    el.style.transition = '0.2s';
+  }
+  if (Math.ceil(scrolled) != scrollable) {
+    var el = document.getElementById("test");
+    el.style.opacity = '1'
+    el.style.transition = '1s';
+  }
 });
