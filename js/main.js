477d68b227f4797a28565bdc5cc329ffd0158128
Junhan <snrtn94@gmail.com>
2021-06-24 15:42:01 +0200

    asd

--- js/main.js
+++ js/main.js
@@ -7,23 +7,6 @@ $(function () {
   });
 });
 
-// subname scroll event
-window.addEventListener('scroll', () => {
-  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
-  const scrolled = window.scrollY;
-
-  if (Math.ceil(scrolled) === scrollable) {
-    var el = document.getElementById("test");
-    el.style.opacity = '0'
-    el.style.transition = '1s';
-  }
-  if (Math.ceil(scrolled) != scrollable) {
-    var el = document.getElementById("test");
-    el.style.opacity = '1'
-    el.style.transition = '2s';
-  }
-});
-
 // image slide auto
 var counter = 1;
 setInterval(function () {
@@ -39,11 +22,28 @@ $(document).ready(function () {
   $(".gallery").hide();
   $(function () {
     $(window).scroll(function () {
-      if ($(this).scrollTop() > 500) {
+      if ($(this).scrollTop() > 400) {
         $('.gallery').fadeIn(1000);
       } else {
         $('.gallery').fadeoOut(1000);
       }
     });
   });
+});
+
+// subname scroll event
+window.addEventListener('scroll', () => {
+  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+  const scrolled = window.scrollY;
+
+  if (Math.ceil(scrolled) === scrollable) {
+    var el = document.getElementById("test");
+    el.style.opacity = '0'
+    el.style.transition = '1s';
+  }
+  if (Math.ceil(scrolled) != scrollable) {
+    var el = document.getElementById("test");
+    el.style.opacity = '1'
+    el.style.transition = '2s';
+  }
 });
