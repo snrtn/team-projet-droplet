7a5b83005e45701854d0793a5019c60273e5e6b8
Junhan <snrtn94@gmail.com>
2021-06-27 22:01:05 +0200

    asdasd
    asdasd

--- js/main.js
+++ js/main.js
@@ -7,6 +7,31 @@ $(function () {
   });
 });
 
+// Header, Navbar SlideDown, Line 23
+$(document).ready(function () {
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 10){
+        $('.test').fadeIn(1000);
+        $('.test').css('background-color','#fff');
+        $('.logo a').css('color','#000');
+        $('.gnb a').css('color','#000');
+        $('.menu a').css('color','#000');
+        $('.side_box i').css('color','#000');
+      }
+      if ($(this).scrollTop() < 9){
+        $('.test').fadeIn(1000);
+        $('.test').css('background-color','#333');
+        $('.logo a').css('color','#fff');
+        $('.gnb a').css('color','#fff');
+        $('.menu a').css('color','#000');
+        $('.side_box i').css('color','#fff');
+        $('.test').removeAttr('a');
+      }
+    });
+  });
+});
+
 // Section02, GalleryText SlideDown, Line 89
 $(document).ready(function () {
   $(".gallery").hide();
@@ -21,3 +46,4 @@ $(document).ready(function () {
     });
   });
 });
+
