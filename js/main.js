c33d6ca550aa3f98b55ef0534dd8161838a12f59
Junhan <snrtn94@gmail.com>
2021-07-05 10:44:16 +0200

    I fixed. small parts
    import Variables, import Navbar, Footbar, about.html, contact.html, all css, main.js, index.html

--- js/main.js
+++ js/main.js
@@ -1,23 +1,13 @@
-// hambuger icon event 
-$(function () {
-  /* header - sidenav */
-  $('.hambuger').click(function () {
-    $(this).toggleClass('active');
-    $('header').toggleClass('active');
-  });
-});
-
 // Header, Navbar SlideDown, Line 23
-// I'm fix it.
 $(document).ready(function () {
   $(function () {
     $(window).scroll(function () {
-      if ($(this).scrollTop() > 10){
+      if ($(this).scrollTop() > 5){
         $('.test').fadeIn();
         $('.test').css('background-color','rgba(51, 51, 51, 0.7)');
         $('#test').css('color','transparent');
       }
-      if ($(this).scrollTop() < 10){
+      if ($(this).scrollTop() < 5){
         $('#test').css('color','#fff');
         $('#test').css('transition','0.5s');
       }
