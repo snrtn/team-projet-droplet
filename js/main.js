a35d696f775c6643adfb1e2f6c632bc75a847e73
Junhan <snrtn94@gmail.com>
2021-07-06 16:23:16 +0200

    maker.html 추가 Navbar.html , index.html 수정 헀습니다.
    maker.html 추가 Navbar.html , index.html 수정 헀습니다.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 $(document).ready(function () {
   $(function () {
     $(window).scroll(function () {
-      if ($(this).scrollTop() > 5){
+      if ($(this).scrollTop() > 5) {
         $('.test').fadeIn();
-        $('.test').css('background-color','rgba(51, 51, 51, 0.7)');
-        $('#test').css('color','transparent');
+        $('.test').css('background-color', 'rgba(51, 51, 51, 0.7)');
+        $('#test').css('color', 'transparent');
       }
-      if ($(this).scrollTop() < 5){
-        $('#test').css('color','#fff');
-        $('#test').css('transition','0.5s');
+      if ($(this).scrollTop() < 5) {
+        $('#test').css('color', '#fff');
+        $('#test').css('transition', '0.5s');
       }
     });
   });
